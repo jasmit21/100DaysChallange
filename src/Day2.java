e754192ba6e38d7f49f52cae4311f770feedfae5
jasmit21 <jasmitrathod21@gmail.com>
2021-09-17 20:15:15 +0530

    Initial Commit

---
+++ src/Day2.java
@@ -0,0 +1,9 @@
+public class Day2 {
+    public static void main(String[] args){
+        int a = 4;
+        int b = 3;
+        if (a%2==0)
+            System.out.println("IT IS EVEN");
+        else System.out.println("THE NUMBER IS ODD");
+    }
+}
